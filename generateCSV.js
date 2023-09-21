b5477227bc08bd9139b2e1d68171b96079809d4d
Luke Padawer <54520310+lukeslab@users.noreply.github.com>
2023-09-21 11:25:02 -0400

    added json-2-csv conversion package. Updated generateCSV.js script.

--- generateCSV.js
+++ generateCSV.js
@@ -1,54 +1,31 @@
-require('dotenv').config()
 const fs = require('fs');
+const converter = require('json-2-csv')
 const { getFilesRecursive } = require('./functions/getFilesRecursive')
 
-const BATCH_NUMBER = ""
+const BATCH_NUMBER = "20230829_15:14:42"
 
 if (!BATCH_NUMBER) throw "No batch specified."
 
-const columnHeaders = [
-    "Filepath",
-    "FIRST_NAME_text", "FIRST_NAME_score", 
-    "LAST_NAME_text", "LAST_NAME_score",
-    "MIDDLE_NAME_text", "MIDDLE_NAME_score",
-    "SUFFIX_text", "SUFFIX_score",
-    "CITY_IN_ADDRESS_text", "CITY_IN_ADDRESS_score",
-    "ZIP_CODE_IN_ADDRESS_text", "ZIP_CODE_IN_ADDRESS_score",
-    "STATE_IN_ADDRESS_text", "STATE_IN_ADDRESS_score",
-    "STATE_NAME_text", "STATE_NAME_score",
-    "DOCUMENT_NUMBER_text", "DOCUMENT_NUMBER_score",
-    "EXPIRATION_DATE_text", "EXPIRATION_DATE_score",
-    "DATE_OF_BIRTH_text", "DATE_OF_BIRTH_score",
-    "DATE_OF_ISSUE_text", "DATE_OF_ISSUE_score",
-    "ID_TYPE_text", "ID_TYPE_score",
-    "ENDORSEMENTS_text", "ENDORSEMENTS_score",
-    "VETERAN_text", "VETERAN_score",
-    "RESTRICTIONS_text", "RESTRICTIONS_score",
-    "CLASS_text", "CLASS_score",
-    "ADDRESS_text", "ADDRESS_score",
-    "COUNTY_text", "COUNTY_score",
-    "PLACE_OF_BIRTH_text", "PLACE_OF_BIRTH_score",
-    "MRZ_CODE_text", "MRZ_CODE_score"
-];
+async function convertJsonToCSV(data, options, path) {
+    const csv = await converter.json2csv(data, options)
+    fs.writeFileSync(path, csv)
+}
 
-const outputFiles = getFilesRecursive(`./batches/${BATCH_NUMBER}/json`)
-const csvFile = `./batches/${BATCH_NUMBER}/results.csv`
+const jsonFiles = getFilesRecursive(`./batches/${BATCH_NUMBER}/json`)
 
-console.log(outputFiles)
+const csvFilepath = `./batches/${BATCH_NUMBER}/results.csv`
 
-fs.appendFileSync(csvFile, `${columnHeaders.toString()}\n`)
+const options = {
+    expandNestedObjects: true,
 
-outputFiles.forEach( file => {
-    const fileFields = JSON.parse(fs.readFileSync(file.pathName, 'utf8'))
-    fs.appendFileSync(csvFile, `${file.pathName},`)
-    fileFields.forEach( field => {
-        if (field.Type.Text === "MRZ_CODE") {
-           const newMRZ_CODEText = field.ValueDetection.Text.replace(/\n/, "\\n")
-           fs.appendFileSync(csvFile, `${field.ValueDetection.Text === "" ? "No text" : newMRZ_CODEText}, ${field.ValueDetection.Confidence},`)
-        } else {
-            fs.appendFileSync(csvFile, `${field.ValueDetection.Text === "" ? "No text" : field.ValueDetection.Text}, ${field.ValueDetection.Confidence},`)
-        }
-    })
-    fs.appendFileSync(csvFile, "\n")
+}
+const jsonObjectsFromFiles = []
+
+jsonFiles.forEach( file => {
+    const json = JSON.parse(fs.readFileSync(file.pathName, 'utf8'))
+    jsonObjectsFromFiles.push(json)
 })
+
+convertJsonToCSV(jsonObjectsFromFiles, options, csvFilepath)
+
 
